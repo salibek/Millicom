1932da81aadfad5f51af23804ae2049ce54b5ce2
Sergey Salibekyan <salibek@yandex.ru>
2026-04-08 13:55:03 +0300

    Console format

--- Console.cpp
+++ Console.cpp
@@ -292,7 +292,6 @@ void Console::ProgFU(long int MK, LoadPoint Load, FU* Sender)
 		break;
 // ......
 
-
 	case 200: // NoVarToOutProgSet Установить подрограмму реакции на ошибку "Нет переменной для ввода"
 		NoVarToOutProg = Load.Point;
 		break;
